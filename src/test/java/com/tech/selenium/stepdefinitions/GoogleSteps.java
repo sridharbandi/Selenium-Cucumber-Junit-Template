32d0b1c7f84b946426246b194c53513808f20289
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-01-28 21:11:06 +0000

    Skeleton Stepdefs

--- src/test/java/com/tech/selenium/stepdefinitions/GoogleSteps.java
+++ src/test/java/com/tech/selenium/stepdefinitions/GoogleSteps.java
@@ -1,4 +1,32 @@
 package com.tech.selenium.stepdefinitions;
 
-public class GoogleSteps {
+import cucumber.api.Scenario;
+import cucumber.api.java.en.Given;
+import cucumber.api.java8.En;
+
+public class GoogleSteps implements En {
+
+    public GoogleSteps() {
+
+        Before((Scenario scenario) -> {
+
+        });
+
+        Given("^User is on Google search page$", () -> {
+
+        });
+
+        When("^User searches for Selenium$", () -> {
+
+        });
+
+        Then("^User can see Selenium results$", () -> {
+
+        });
+
+        After((Scenario scenario) -> {
+
+        });
+
+    }
 }
