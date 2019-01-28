d7d56bec2f77a7c6ec6abcdc5d3bfaca34231f38
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-01-28 21:14:08 +0000

    Implemented business logics

--- src/test/java/com/tech/selenium/stepdefinitions/GoogleSteps.java
+++ src/test/java/com/tech/selenium/stepdefinitions/GoogleSteps.java
@@ -1,31 +1,45 @@
 package com.tech.selenium.stepdefinitions;
 
+import com.tech.selenium.driverutil.DriverFactory;
+import com.tech.selenium.pageobjects.GoogleSearchPage;
+import com.tech.selenium.pageobjects.SearchResultsPage;
 import cucumber.api.Scenario;
 import cucumber.api.java.en.Given;
 import cucumber.api.java8.En;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
 
+/**
+ * Created by Sridhar Bandi on 28/01/19.
+ */
 public class GoogleSteps implements En {
+
+    private WebDriver driver;
+    GoogleSearchPage googleSearchPage;
+    SearchResultsPage searchResultsPage;
 
     public GoogleSteps() {
 
         Before((Scenario scenario) -> {
-
+            driver = DriverFactory.getBrowser();
+            googleSearchPage = new GoogleSearchPage(driver);
+            searchResultsPage = new SearchResultsPage(driver);
         });
 
         Given("^User is on Google search page$", () -> {
-
+            googleSearchPage.openURL();
         });
 
         When("^User searches for Selenium$", () -> {
-
+            googleSearchPage.searchFor("Selenium");
         });
 
         Then("^User can see Selenium results$", () -> {
-
+            Assert.assertTrue(searchResultsPage._lnkSelenium.isDisplayed());
         });
 
         After((Scenario scenario) -> {
-
+            driver.quit();
         });
 
     }
